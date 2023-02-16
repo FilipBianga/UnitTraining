87f0819b9ab4c9ba077428e02b88d364ea0dee1f
FilipBianga <fbianga94@gmail.com>
2023-02-16 20:06:32 +0100

    Dynamic Tests

--- src/main/java/com/example/unittraining/section_one/Meal.java
+++ src/main/java/com/example/unittraining/section_one/Meal.java
@@ -10,10 +10,16 @@ import java.util.Objects;
 public class Meal {
 
     private int price;
+    private int quantity;
     private String name;
 
     public Meal(int price) {
         this.price = price;
+    }
+
+    public Meal(int price, String name) {
+        this.price = price;
+        this.name = name;
     }
 
     public int getDiscountedPrice(int discount) {
